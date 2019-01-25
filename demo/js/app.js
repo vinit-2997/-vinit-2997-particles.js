df41dfde8f8527ca699b88015f41bf65116bcd05
Vinit Todai <vinit.7todai@gmail.com>
2019-01-25 19:06:39 +0530

    Update app.js

--- demo/js/app.js
+++ demo/js/app.js
@@ -135,7 +135,7 @@ window.addEventListener("load",(e) => {
     console.log("called");
   if ('serviceWorker' in navigator) {
     try {
-      navigator.serviceWorker.register('./../../serviceworker.js');
+      navigator.serviceWorker.register('./serviceworker.js');
       console.log('SW registered');
     } catch (error) {
       console.log('SW failed');
